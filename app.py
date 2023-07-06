d810bf1ef058caba48492d12d9fd00726aaa702b
pratapshingane18 <sushrutrao465@gmail.com>
2023-07-06 12:22:43 +0530

    Woeking on fronend fileinput

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
 import streamlit as st
+
+st.sidebar.title("Whatsapp Chat Analyzer")
+
+uploaded_file = st.sidebar.file_uploader("Choose a file")
+if uploaded_file is not None:
+    bytes_data = uploaded_file.getvalue()
+    data = bytes_data.decode("utf-8")
